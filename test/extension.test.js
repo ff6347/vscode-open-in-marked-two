0141c5593844bea6c313aef00969db6adf28e4e0
fmoronzirfas <fabian.moron-zirfas@ixds.com>
2018-10-24 09:10:45 +0200

    test(update and testrunner):

--- test/extension.test.js
+++ test/extension.test.js
@@ -6,25 +6,20 @@
 //
 
 // The module 'assert' provides assertion methods from node
-// const assert = require('assert');
+const assert = require('assert');
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-// const vscode = require('vscode');
-// const myExtension = require('../extension');
+const vscode = require('vscode');
+const myExtension = require('../extension');
 
 // Defines a Mocha test suite to group tests of similar kind together
-// suite("Extension Tests", function() {
+suite("Extension Tests", function() {
 
 //     // Defines a Mocha unit test
-//     test("Something 1", function() {
-//         assert.equal(-1, [1, 2, 3].indexOf(5));
-//         assert.equal(-1, [1, 2, 3].indexOf(0));
-//     });
-// });
+    test("Something 1", function() {
+        assert.equal(-1, [1, 2, 3].indexOf(5));
+        assert.equal(-1, [1, 2, 3].indexOf(0));
+    });
+});
 
-describe('extension', ()=>{
-    it('should be a test',()=>{
-        expect(1).toEqual(1);
-    })
-})
